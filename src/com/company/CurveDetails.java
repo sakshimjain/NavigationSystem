0701f0de8465c46c336d93f55813316e3efa2288
sakshi jain <3135sakshi@gmail.com>
2020-11-23 15:41:30 -0700

    formatting code

--- src/com/company/CurveDetails.java
+++ src/com/company/CurveDetails.java
@@ -2,49 +2,60 @@ package com.company;
 
 public class CurveDetails {
 
-	public double timeOffset;
+    public double timeOffset;
     public String startPoint;
     public String endPoint;
     public String direction;
     public String speedWarning;
     public Double avgSpeed;
-	public double getTimeOffset() {
-		return timeOffset;
-	}
-	public void setTimeOffset(double timeOffset) {
-		this.timeOffset = timeOffset;
-	}
-	public String getStartPoint() {
-		return startPoint;
-	}
-	public void setStartPoint(String startPoint) {
-		this.startPoint = startPoint;
-	}
-	public String getEndPoint() {
-		return endPoint;
-	}
-	public void setEndPoint(String endPoint) {
-		this.endPoint = endPoint;
-	}
-	public String getDirection() {
-		return direction;
-	}
-	public void setDirection(String direction) {
-		this.direction = direction;
-	}
-	public String getSpeedWarning() {
-		return speedWarning;
-	}
-	public void setSpeedWarning(String speedWarning) {
-		this.speedWarning = speedWarning;
-	}
-	public Double getAvgSpeed() {
-		return avgSpeed;
-	}
-	public void setAvgSpeed(Double avgSpeed) {
-		this.avgSpeed = avgSpeed;
-	}
-    
-    
-    
+
+    public double getTimeOffset() {
+        return timeOffset;
+    }
+
+    public void setTimeOffset(double timeOffset) {
+        this.timeOffset = timeOffset;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getSpeedWarning() {
+        return speedWarning;
+    }
+
+    public void setSpeedWarning(String speedWarning) {
+        this.speedWarning = speedWarning;
+    }
+
+    public Double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public void setAvgSpeed(Double avgSpeed) {
+        this.avgSpeed = avgSpeed;
+    }
+
+
 }
